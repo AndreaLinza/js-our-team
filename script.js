658d8e272490a3cfa9a68169ad6590e6b4c34666
AndreaLinza <andrealinza@gmail.com>
2023-07-15 00:35:21 +0200

    card member random and structure

--- script.js
+++ script.js
@@ -1,53 +1,94 @@
-/*const mainDiv = document.createElement("div")
-mainDiv.classList.add("card")
-mainDiv.style.width = "18rem"
 
-const insideDiv = document.createElement("div")
-insideDiv.classList.add("card-body")
+// const yearMember
 
-const textTeam = document.createElement("p")
-textTeam.classList.add("card-text")*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 const containerCard = document.querySelector(".container-card")
-const card = divCard(divBody(textCard()))
+const btnAdd = document.getElementById("btn-add")
 
 
 
-console.log(card)
+btnAdd.addEventListener("click", function () {
+    const addCard = divCard(inputMember)
 
-function divCard(body,text){
-    
+})
+
+function divCard(memberTeam) {
+
+    if (memberTeam.value === "" || memberTeam.value === "undefined"){
+        return
+    }
+
         const mainDiv = document.createElement("div")
-        mainDiv.classList.add("card")
-        mainDiv.style.width = "18rem"
-        containerCard.append(mainDiv)
+    mainDiv.classList.add("card")
+    mainDiv.style.width = "30.3%"
+    let random = Math.floor(Math.random() * 100)
+    mainDiv.innerHTML = `<img src="https://picsum.photos/350/350?random=${random}" alt="student"></img>`
 
-        //console.log(mainDiv)
-        return mainDiv
+    const bodyCard = document.createElement("div")
+    bodyCard.classList.add("card-body")
+
+
+    mainDiv.append(bodyCard)
+    bodyCard.append(inputMember())
+    containerCard.append(mainDiv)
+
+    return mainDiv
+
+
+}
+
+
+function inputMember() {
+
+    let inputText = document.querySelector("[type='text']")
+    const newMember = {}///////
+    if(inputText.value === "" || inputText.value === "undefined"){
+        alert("Inserire Nome Valido")
+        window.location.reload()
     }
+    const textTeam = document.createElement("p") ///// mi da come input solo uno risolvere
+    textTeam.classList.add("card-text")
+    textTeam.append(inputText.value)
 
-    console.log(divCard())
+    console.log(inputText.value)
 
-    function divBody(text){
+    return textTeam
+}
 
-        const insideDiv = document.createElement("div")
-        insideDiv.classList.add("card-body")
-        divCard().append(insideDiv)
 
-        //console.log(insideDiv)
-    
-        return insideDiv
-    }
-
-    function textCard(){
-
-        const textTeam = document.createElement("p")
-        textTeam.classList.add("card-text")
-        divBody().append(textTeam)
-
-        //console.log(textTeam)
-    
-        return textTeam
-        
-    }
